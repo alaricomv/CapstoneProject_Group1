25a02146ea01d8291b33e308adecf28197971202
elaheh2024 <e.dehghan@student.fdu.edu>
2024-02-21 13:43:18 -0800

    Modified html and css in the home page, created header

---
+++ frontend/wholesaleconnect/src/app/components/partials/headerexample/headerexample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headerexample',
+  standalone: true,
+  imports: [],
+  templateUrl: './headerexample.component.html',
+  styleUrl: './headerexample.component.css'
+})
+export class HeaderexampleComponent {
+
+}
