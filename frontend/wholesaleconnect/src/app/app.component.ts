c942a5fa43ab9b8bf882c250e9480cca7e734f13
Eloise Chen <eloisechenzxc@icloud.com>
2024-02-23 12:47:31 -0800

    Created cartpage component and implemented order summary to be further connected to homepage via routing

--- frontend/wholesaleconnect/src/app/app.component.ts
+++ frontend/wholesaleconnect/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HeaderexampleComponent } from './components/partials/headerexample/headerexample.component';
-
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 
 @Component({
   selector: 'app-root',
 
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,HeaderexampleComponent],
+  imports: [RouterOutlet,HomeComponent,HeaderexampleComponent,CartPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
