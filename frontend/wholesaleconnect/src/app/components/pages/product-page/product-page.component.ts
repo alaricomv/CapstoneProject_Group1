d9db2d707225ff58ea8333bec087021195dd6cff
Eloise Chen <eloisechenzxc@icloud.com>
2024-03-26 20:09:04 -0700

    Implemented changes including layout adjustments, adding 'add to cart' button, and introducing NG-ZORRO.

--- frontend/wholesaleconnect/src/app/components/pages/product-page/product-page.component.ts
+++ frontend/wholesaleconnect/src/app/components/pages/product-page/product-page.component.ts
@@ -1,32 +1,34 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { ProductService } from '../../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.css'
+  styleUrl: './product-page.component.css',
 })
-export class ProductPageComponent implements OnInit{
-
+export class ProductPageComponent implements OnInit {
   product!: Product;
-  constructor(activatedRoute: ActivatedRoute, productService:ProductService) {
+  constructor(
+    activatedRoute: ActivatedRoute,
+    productService: ProductService,
+    private message: NzMessageService
+  ) {
     activatedRoute.params.subscribe((params) => {
-      productService.getProductById(params['id']).subscribe(serverProduct =>{
-        console.log("here");
+      productService.getProductById(params['id']).subscribe((serverProduct) => {
+        console.log('here');
         console.log(serverProduct);
         this.product = serverProduct;
       });
-    })
-
+    });
   }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  addToCart() {
+    this.message.info('In development');
   }
-
-
 }
